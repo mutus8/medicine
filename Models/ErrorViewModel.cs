a2923fec001e507b3a89029bce994ba8afbf6ce4
Your Name <you@example.com>
2025-03-31 20:15:06 +0200

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace medicine.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
